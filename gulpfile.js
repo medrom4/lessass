bbdbb05a42763d3dcfd9be7dbc142f12e39083d9
Ramanovich <medikroma@gmail.com>
2018-12-21 14:57:33 +0000

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
+
+//gulp.task('mytask', function(){
+//    return gulp.src('source-files')
+//        .pipe(plugin())
+//        .pipe(gulp.dest('folder'))
+//});
+
+gulp.task('sass', function() {
+   return gulp.src('app/sass/main.sass').pipe(sass()).pipe(gulp.dest('app/css'))
+});
