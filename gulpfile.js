e11e3df3916abe98af87671b6d5d58daac09ff88
Ramanovich <medikroma@gmail.com>
2018-12-25 17:34:13 +0000

    js-concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,73 @@
 var gulp = require('gulp'), // Подключаем Gulp
-	sass = require('gulp-sass'), //Подключаем Sass пакет
-	browserSync = require('browser-sync'), // Подключаем Browser Sync
-	concat = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
-	uglify = require('gulp-uglifyjs') // Подключаем gulp-uglifyjs (для сжатия JS)
+    sass = require('gulp-sass'), //Подключаем Sass пакет
+    browserSync = require('browser-sync'), // Подключаем Browser Sync
+    concat = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
+    uglify = require('gulp-uglifyjs'), // Подключаем gulp-uglifyjs (для сжатия JS)
+    concatCss = require('gulp-concat-css'), // соединяет все сss в один
+    cleanCSS = require('gulp-clean-css'), // минифицирует css
+    rename = require("gulp-rename"), // переименование итогового файла css
+    uncss = require('gulp-uncss'); // удалить неиспользуемые css
+
+gulp.task('gconcatcss', function () {
+    return gulp.src('app/css/**/*.css')
+        .pipe(concatCss("assets/bundle.css"))
+        .pipe(cleanCSS({
+            compatibility: 'ie8'
+        }))
+        .pipe(uncss({
+            html: ['app/index.html']
+        }))
+        .pipe(rename("bundle.min.css"))
+        .pipe(gulp.dest('app/assets/css'));
+});
+
+
+gulp.task('scriptsjs', function() {// конкатинируем все js в один
+  return gulp.src('app/js/*.js')
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('app/assets/js'));
+});
+
 
 
 gulp.task('sass', function () { // Создаем таск Sass
-	return gulp.src('app/sass/**/*.sass') // Берем источник
-		.pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
-		.pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
-		.pipe(browserSync.reload({
-				stream: true})) // Обновляем CSS на странице при изменении
-	});
+    return gulp.src('app/sass/**/*.sass') // Берем источник
+        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+        .pipe(browserSync.reload({
+            stream: true
+        })) // Обновляем CSS на странице при изменении
+});
 
 
-gulp.task('scripts', function() {
-	return gulp.src([ // Берем все необходимые библиотеки
+gulp.task('scripts', function () {
+    return gulp.src([ // Берем все необходимые библиотеки
 		'app/libs/jquery/dist/jquery.min.js', // Берем jQuery
 		'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js' // Берем Magnific Popup
 	])
-	.pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
-	.pipe(uglify()) // Сжимаем JS файл
-	.pipe(gulp.dest('app/js')); // Выгружаем в папку app/js
+        .pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
+        .pipe(uglify()) // Сжимаем JS файл
+        .pipe(gulp.dest('app/js')); // Выгружаем в папку app/js
 });
 
 
 gulp.task('browser-sync', function () { // Создаем таск browser-sync
-	browserSync({ // Выполняем browserSync
-		server: { // Определяем параметры сервера
-			baseDir: 'app' // Директория для сервера - app
-		},
-		notify: false // Отключаем уведомления
-	});
-//	browserSync.watch('app/**/*.*').on('change', browserSync.reload);
+    browserSync({ // Выполняем browserSync
+        server: { // Определяем параметры сервера
+            baseDir: 'app' // Директория для сервера - app
+        },
+        notify: false // Отключаем уведомления
+    });
+    //	browserSync.watch('app/**/*.*').on('change', browserSync.reload);
 });
 
 gulp.task('watch', gulp.parallel('browser-sync', 'sass', function () {
-	gulp.watch('app/sass/**/*.sass', ['sass']); // Наблюдение за sass файлами в папке sass
-	gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
-	gulp.watch('app/js/**/*.js', browserSync.reload); // Наблюдение за JS файлами в папке js
+    gulp.watch('app/sass/**/*.sass', ['sass']); // Наблюдение за sass файлами в папке sass
+    gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+    gulp.watch('app/js/**/*.js', browserSync.reload); // Наблюдение за JS файлами в папке js
 }));
 
- // gulp.task('default', gulp.parallel('watch', 'browser-sync'));
+// gulp.task('default', gulp.parallel('watch', 'browser-sync'));
 
 
 
@@ -66,5 +92,3 @@ gulp.task('watch', gulp.parallel('browser-sync', 'sass', function () {
 //bower i jquery magnific-popup /* устанавливаем jQ и mag-popup */
 
 //npm i gulp-concat gulp-uglifyjs --save-dev /* gulp-concat (для конкатенации файлов) 														gulp-uglifyjs (для сжатия JS) */
-
-
